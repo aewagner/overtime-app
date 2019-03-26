acc429d2632e65f19d18eda747dc7df4f0c6ff19
Austin Wagner <austin.e.wagner@gmail.com>
2019-03-26 12:01:34 -0700

    Implemented tests and page redirection for permission structure.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -17,5 +17,13 @@ FactoryGirl.define do
     email { generate :email }
     password "asdfasdf"
     password_confirmation "asdfasdf"
-   end
+  end
+
+  factory :non_authorized_user, class: "User" do
+    first_name "Non"
+    last_name "Authorized"
+    email { generate :email }
+    password "asdfasdf"
+    password_confirmation "asdfasdf"
+  end
 end
