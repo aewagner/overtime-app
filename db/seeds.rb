f60180659e69eec73e0799b128be3050be92d9f2
Austin Wagner <austin.e.wagner@gmail.com>
2019-04-29 14:15:11 -0700

    Formatting updates for seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,13 +29,13 @@ AdminUser.create(
 puts "1 Admin User created"
 
 100.times do |post|
-    Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 posts created"
 
 100.times do |audit_log|
-    AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 end
 
 puts "100 audit logs created"
