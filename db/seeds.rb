c01bc4df2e0eba151005c518ed2ff9225e5d5c1f
Austin Wagner <austin.e.wagner@gmail.com>
2019-08-14 13:00:05 -0700

    Brought in live data for admin review page

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,9 @@ AdminUser.create(
 puts "1 Admin User created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} Hodor. Hodor hodor, hodor. Hodor hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor; hodor 
+  hodor hodor. Hodor. Hodor hodor; hodor hodor - hodor, hodor, hodor hodor. Hodor, hodor. Hodor. Hodor, hodor hodor hodor; hodor hodor; hodor hodor 
+  hodor! Hodor hodor HODOR! Hodor hodor... Hodor hodor hodor...", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 posts created"
